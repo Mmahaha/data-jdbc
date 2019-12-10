191bb1d4238123b57f164e42b3ddf86324c848d7
linxiaotang031702542 <2050556217@qq.com>
2019-12-10 21:41:32 +0800

    增加展示词条接口和根据词条ID获取信息接口

--- src/main/java/com/example/datajdbc/mapper/InfoMapper.java
+++ src/main/java/com/example/datajdbc/mapper/InfoMapper.java
@@ -3,6 +3,9 @@ package com.example.datajdbc.mapper;
 import com.example.datajdbc.bean.Info;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+import java.util.Map;
+
 public interface InfoMapper {
      /*
     Edited by 伟欢
@@ -18,7 +21,12 @@ public interface InfoMapper {
     展示词条列表(只要显示词条标题和词条id)；
     根据词条id获取具体信息；
      */
-
+    //展示词条列表
+    @Select("select infoId,entry from info")
+    public List<Map>getAllInfo();
+    //根据词条id获取具体信息
+    @Select("select * from info where infoId=#{infoId}")
+    public Info getInfoById(Integer infoId);
 
 
 
