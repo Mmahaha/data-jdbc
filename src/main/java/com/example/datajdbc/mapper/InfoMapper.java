7f27031872cd148961471b3fd91eb64027319f2f
“your_suweihuangit config --global user.email “suweihuan079243@163.comgit config --global user.name “your_suweihuan <suweihuan079243@163.com>
2019-12-10 22:08:57 +0800

    创建词条修改词条

--- src/main/java/com/example/datajdbc/mapper/InfoMapper.java
+++ src/main/java/com/example/datajdbc/mapper/InfoMapper.java
@@ -3,12 +3,24 @@ package com.example.datajdbc.mapper;
 import com.example.datajdbc.bean.Info;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 public interface InfoMapper {
      /*
     Edited by 伟欢
     创建词条；
     修改词条；
      */
+
+    @Options(useGeneratedKeys = true, keyProperty = "infoId")   //能够使自增的taskId返回数值
+    @Insert("insert into info(userId,entry,content,creatAt) values(#{userId},#{entry},#{content},now())")
+    public void creatInfo(Info info);
+
+    @Select("select * from info")
+    public List<Info> selectAllInfos();
+
+    @Update("update info set entry=#{entry},content=#{content},refreshAt=now() where infoId=#{infoId}")
+    public void updateInfo(Info info);
 
 
 
